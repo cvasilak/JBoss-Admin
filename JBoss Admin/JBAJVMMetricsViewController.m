057dfb101e4a04d8b6955bd15e953a67d79a7c4b
Christos Vasilakis <cvasilak@gmail.com>
2012-05-14 20:31:42 +0300

    update sources with license header, added file to include list of contributors

--- JBoss Admin/JBAJVMMetricsViewController.m
+++ JBoss Admin/JBAJVMMetricsViewController.m
@@ -1,10 +1,24 @@
-//
-//  JBAJVMMetricsViewController.m
-//  JBoss Admin
-//
-//  Author: Christos Vasilakis <cvasilak@gmail.com>
-//  Copyright 2012 All rights reserved.
-//
+/*
+ * JBoss Admin
+ * Copyright 2012, Christos Vasilakis, and individual contributors.
+ * See the copyright.txt file in the distribution for a full
+ * listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 
 #import "JBAJVMMetricsViewController.h"
 #import "JBossValue.h"
